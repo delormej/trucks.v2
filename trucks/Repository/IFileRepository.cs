6b20946f0cd8c9deac09d8c8a4316c98b9702cb5
Jason De Lorme <delormej@gmail.com>
2022-02-12 07:57:51 -0500

    Comments.

--- trucks/Repository/IFileRepository.cs
+++ trucks/Repository/IFileRepository.cs
@@ -1,7 +1,19 @@
 namespace Trucks
 {
+    /// <summary>
+    /// Defines the interface for where persistent unstructured storage should be used as
+    /// files are downloaded and converted, but before canonicalized into structured / nosql
+    /// persistence.  
+    /// </summary>
+    /// <notes>
+    /// Example implementations include local disk and cloud storage. 
+    /// </notes>
     public interface IFileRepository
     {
+        /// <summary>
+        /// Takes the local file name, storages to the destination and returns the 
+        /// destination path.
+        /// </summary>
         Task<string> SaveAsync(string localPath);
     }
 }
