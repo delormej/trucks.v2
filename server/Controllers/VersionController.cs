6d57c43d81abc3102fc6e28eec0c80e61e1ee27e
Jason De Lorme <delormej@gmail.com>
2022-08-01 10:01:45 -0400

    Adding version controller.

---
+++ server/Controllers/VersionController.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trucks.Server
+{
+    public record VerisonInfo
+    (
+        string ProjectId,
+        string Version,
+        string ComputeInstanceId,
+        string ServiceRevision
+    );
+
+    [Route("[controller]")]
+    public class VersionController : Controller
+    {
+        readonly VerisonInfo _version;
+
+        public VersionController(IConfiguration config)
+        {
+            _version = new VerisonInfo
+            (
+                ProjectId: config["ProjectId"],
+                ComputeInstanceId: config["InstanceId"],
+                Version: GetVersion(),
+                ServiceRevision: config["ServiceRevision"]
+            );
+        }
+
+        [AllowAnonymous]
+        [HttpGet()]
+        public VerisonInfo Version()
+        {
+            return _version;
+        }
+
+        private string GetVersion()
+        {
+            try 
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                    .InformationalVersion;
+            }
+            catch 
+            {
+                return "";
+            }
+        }
+    }
+}
